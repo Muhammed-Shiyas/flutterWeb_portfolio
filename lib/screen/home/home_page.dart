454359973b0ff4642eab35b5f3b4f8850d1acc8a
Muhammed-Shiyas <muhammed.shiyas.005@gmail.com>
2024-01-10 11:57:18 +0530

    first commit

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:portfolio/consts/data.dart';
 import 'package:portfolio/providers/current_state.dart';
+
 import 'package:portfolio/screen/home/phone_screen_wrapper.dart';
 
 import 'package:provider/provider.dart';
@@ -13,110 +14,102 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    CurrentState currentState =
+        Provider.of<CurrentState>(context, listen: false);
     bool isMobile =
         MediaQuery.of(context).size.width < 600; // Adjust the width as needed
 
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          Size size = constraints.biggest;
-          CurrentState currentState =
-              Provider.of<CurrentState>(context, listen: false);
-
-          return Stack(
-            children: [
-              if (!isMobile)
-                const SizedBox(
-                    width: double.infinity,
-                    height: double.infinity,
-                    child: RiveAnimation.asset(
-                      'assets/space.riv',
-                      fit: BoxFit.cover,
-                    )),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: size.height - 80,
-                        child: Consumer<CurrentState>(
-                          builder: (context, _, __) {
-                            return DeviceFrame(
-                              device: currentState.currentDevice,
-                              screen: ScreenWrapper(
-                                childG: currentState.currentScreen,
-                              ),
-                            );
-                          },
+        body: Stack(
+      children: [
+        if (!isMobile)
+          const SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: RiveAnimation.asset(
+                'assets/space.riv',
+                fit: BoxFit.cover,
+              )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height - 80,
+                  child: Consumer<CurrentState>(
+                    builder: (context, _, __) {
+                      return DeviceFrame(
+                        device: currentState.currentDevice,
+                        screen: ScreenWrapper(
+                          childG: currentState.currentScreen,
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      // Generating the List of Icons
-                      ...List.generate(
-                        devices.length,
-                        (index) => Selector<CurrentState, DeviceInfo>(
-                          selector: (context, provider) =>
-                              provider.currentDevice,
-                          builder: (context, _, __) {
-                            // Creating CustomButtons
-                            return CustomButton(
-                              height: 38,
-                              width: 38,
-                              borderRadius: 100,
-                              backgroundColor: Colors.black,
-                              onPressed: () {
-                                currentState.changeSelectedDecice(
-                                    devices[index].device);
-                              },
-                              isThreeD: true,
-                              animate: true,
-                              pressed: currentState.currentDevice ==
-                                      devices[index].device
-                                  ? Pressed.pressed
-                                  : Pressed.notPressed,
-                              shadowColor: Colors.white,
-                              child: Center(
-                                child: Icon(
-                                  devices[index].data,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: Align(
-                  alignment: Alignment(0, 1),
-                  child: SizedBox(
-                    height: 30,
-                    width: 200,
-                    child: Center(
-                      child: Text(
-                        'passcode is 1234',
-                        style: TextStyle(fontSize: 20, color: Colors.black),
-                      ),
-                    ),
+                      );
+                    },
                   ),
                 ),
-              )
-            ],
-          );
-        },
-      ),
-    );
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // Generating the List of Icons
+                ...List.generate(
+                  devices.length,
+                  (index) => Selector<CurrentState, DeviceInfo>(
+                    selector: (context, provider) => provider.currentDevice,
+                    builder: (context, _, __) {
+                      // Creating CustomButtons
+                      return CustomButton(
+                        height: 38,
+                        width: 38,
+                        borderRadius: 100,
+                        backgroundColor: Colors.black,
+                        onPressed: () {
+                          currentState
+                              .changeSelectedDecice(devices[index].device);
+                        },
+                        isThreeD: true,
+                        animate: true,
+                        pressed:
+                            currentState.currentDevice == devices[index].device
+                                ? Pressed.pressed
+                                : Pressed.notPressed,
+                        shadowColor: Colors.white,
+                        child: Center(
+                          child: Icon(
+                            devices[index].data,
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.only(bottom: 20),
+          child: Align(
+            alignment: Alignment(0, 1),
+            child: SizedBox(
+              height: 30,
+              width: 200,
+              child: Center(
+                child: Text(
+                  'passcode is 1234',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
